5e8739a64a17e75b9cd3593ec21e85d6eda91dcd
a.khachatryan <asya.khachatryan@ocado.com>
2025-03-13 12:10:25 +0000

    task02: Finding the shortest path in a weighted graph

--- src/main/java/org/andersen/Main.java
+++ src/main/java/org/andersen/Main.java
@@ -1,7 +1,15 @@
 package org.andersen;
 
+import org.andersen.service.ShortestPathFinderService;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Andersen");
+        ShortestPathFinderService shortestPathFinderService = new ShortestPathFinderService();
+        int[][] graph = {{1, 2, 5}, {1, 3, 10}, {2, 4, 2}, {3, 4, 1}, {4, 5, 3}};
+        List<Integer> shortestPath = shortestPathFinderService.findShortestPath(graph, 1, 5);
+
+        System.out.println("Output: " + shortestPath);
     }
 }
